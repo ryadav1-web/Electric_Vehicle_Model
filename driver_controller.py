6587d0b82e9c5cdd45b0d17e59e8b155eb1584fc
Rajeshwar Yadav <ryadav1@mtu.edu>
2025-12-16 14:36:53 -0500

    1.0.1: Adding anti windup logic to remove brake torque request at standstill

--- driver_controller.py
+++ driver_controller.py
@@ -12,10 +12,16 @@ def driver_command(veh_speed, v_ref):
     global integral_error
 
     error = v_ref - veh_speed
-    integral_error += error * delta_time
 
-    # PI controller
-    controller_output = Kp * error + Ki * integral_error
+    # PI Controller with Anti windup to remove standstill controller output
+    if abs(veh_speed) < 0.05 and abs(v_ref) < 0.05:
+        integral_error = 0.0
+        controller_output = 0.0
+    else:
+        integral_error += error * delta_time
+        controller_output = Kp * error + Ki * integral_error
+
+
 
     if controller_output >= 0:
         throttle = min(controller_output, 1.0)
@@ -23,5 +29,7 @@ def driver_command(veh_speed, v_ref):
     else:
         throttle = 0.0
         brake = min(-controller_output, 1.0)
+    
+    
 
     return throttle, brake
